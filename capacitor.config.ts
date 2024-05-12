9c137c0330bb99108f4bedc1c05ede23e4e37a6c
Tiestof <tiestof@gmail.com>
2024-05-12 01:40:54 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.evaluacion3v2',
+  appName: 'evaluacion3v2',
+  webDir: 'www'
+};
+
+export default config;
